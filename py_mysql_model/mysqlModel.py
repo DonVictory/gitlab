53f47a158b126cfad7cd419fc78dac2de1608854
DonVictory <vitory.dong@gmail.com>
2018-02-07 09:55:34 +0800

    重写update方法

--- py_mysql_model/mysqlModel.py
+++ py_mysql_model/mysqlModel.py
@@ -1,12 +1,12 @@
 # -*- coding = utf8 -*-
+# version = 2.7
 # pymysqlModel:insert del update select
-# py-version = 2.7
+
 import time
 import pymysql
+__author__ = 'Drw'
 
-__authro__ = 'Drw'
-
-class mysqlModel(object):
+class mysqlmodel(object):
     # init
     def __init__(self,host,user,passwd,dbname,port = 3306,charset = 'utf8'):
         self.conn = pymysql.Connect(
@@ -30,20 +30,24 @@ class mysqlModel(object):
             where = " where "+where
         selSql = "select %s from %s %s" % (field,tbName,where)
         return self.excute(selSql)
-    
+
     # update
-    def update(self, field, value,tbName, where):
+    def update(self, keyValues,tbName, where):
+        setValue = ''
+        for k,v in keyValues.items():
+            setValue += '`%s`="%s",' % (k,v)
         if where:
             where = " where "+where
-        updateSql = "update %s set %s=%s %s" % (tbName,field,value,where)
+        updateSql = "update %s set %s %s" % (tbName,setValue[:-1],where)
         return self.excute(updateSql)
-    
     # delete
     def delete(self,tbName, where):
         if where:
             where = " where "+where
         delSql = "delete from %s %s" % (tbName,where)
         return self.excute(delSql)
+
+
 
     # execute
     def excute(self, sql):
@@ -61,7 +65,7 @@ class mysqlModel(object):
             print(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + '--' + str(e))
             return False
 
-    # close
+    # __del__
     def __del__(self):
         self.cursor.close()
         self.conn.close()
